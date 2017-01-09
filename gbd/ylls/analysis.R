19f0d5dd85260aaf2e9550b5d1d1d7e58fe2d288
Saki Uwagawa <uwagawas@uw.edu>
2017-01-09 11:45:47 -0800

    worked on analysis.R

--- gbd/ylls/analysis.R
+++ gbd/ylls/analysis.R
@@ -1,8 +1,9 @@
 # A brief look at self harm data, downloaded from http://ihmeuw.org/3zn8
 
 # Set up 
+install.packages("dplyr")
 library(dplyr)
-setwd('~/Documents/info-498c/demos/gbd/ylls/')
+setwd('~/Documents/UW/Courses/2016-17/Winter/INFO_498/demos/gbd/ylls/')
 
 # Set margin spacing for graphics
 par(mar=c(5,4,4,2))
@@ -20,9 +21,10 @@ self.harm.data <- self.harm.data %>%
 ###########################################################################
 
 # Subset the data
+death.data <- self.harm.data %>% filter(Measure == 'Deaths per 100,000')
 
 # Create chart
-
+plot(death.data$age.group, death.data$Value)
 
 
 #######################################################################
@@ -30,5 +32,7 @@ self.harm.data <- self.harm.data %>%
 #######################################################################
 
 # Subset the data
+yll.data <- self.harm.data %>% filter(Measure != 'Deaths per 100,000')
 
 # Create chart
+plot(yll.data$age.group, yll.data$Value)
