ac39e1b0db48b75efb835102f6f9ec75dafd54a5
Saki Uwagawa <uwagawas@uw.edu>
2017-02-13 10:55:53 -0800

    Made local changes

--- nhanes/data-prep.R
+++ nhanes/data-prep.R
@@ -1,4 +1,11 @@
+setwd('~/Documents/UW/Courses/2016-17/Winter/INFO_498/demos/nhanes')
+
 install.packages("SASxport")
 
 require(SASxport)
 
+my_data <- read.xport('data/raw/PAQ_H.XPT')
+demo_data <- read.xport('data/raw/DEMO_H.XPT')
+combined <- merge(my_data, demo_data)
+
+write.csv(combined, file = '/data/prepped/my_data.csv')
